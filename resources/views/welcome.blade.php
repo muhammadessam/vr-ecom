4560d45f60757d44e58a6c3fd1af672ec7ee6a73
Muhammad Essam <mohammedessam699@gmail.com>
2020-08-03 13:02:03 +0200

    initial try

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>three.js css3d - youtube</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <style>
+        body {
+            background-color: #ffffff;
+        }
+
+        #blocker {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+        }
+    </style>
+</head>
+<body>
+<div id="container"></div>
+<div id="blocker"></div>
+
+
+<script src="{{asset('js/app.js')}}"></script>
+<script src="{{asset('js/threemainApp.js')}}"></script>
+</body>
+</html>
